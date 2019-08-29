279c0cf68ab3875b775d8a141411d5623f1aa8f1
gmullins925 <50452654+gmullins925@users.noreply.github.com>
2019-08-29 08:39:42 -0500

    Initial Commit

---
+++ Lab10-FiveCardDraw/Card.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Example_VS
+{
+	class Card
+	{
+
+		//card data
+		string rank;
+		string suit;
+		int val;
+
+		/// <summary>
+		/// Gets or sets an integer valuefor the card. Different card games
+		/// would assign different values.
+		/// </summary>
+
+
+		/// <summary>
+		/// Creates a Card object with the given card rank and suit
+		/// </summary>
+		/// <param name="rank">A, K, Q, J, 10...2</param>
+		/// <param name="suit">Use ASCII char for club, spade, heart or diamond</param>
+		public Card(string rank, string suit, int value)
+		{
+			this.Rank = rank;
+			this.Suit = suit;
+			this.val = value;
+		}
+
+		public string Rank { get => rank; set => rank = value; }
+		public string Suit { get => suit; set => suit = value; }
+		public int Value { get => val; set => val = value; }
+
+		public override string ToString()
+		{
+			return Rank + " " + Suit;
+		}
+
+		public static implicit operator Card(DeckOfCards v)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ Lab10-FiveCardDraw/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Example_VS
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			/*
+			Console.OutputEncoding = System.Text.Encoding.Unicode;
+			Card myCard = new Card("Q", "♠", 14);
+			Console.WriteLine(myCard.Value);
+			myCard.Value = 10;
+			Console.WriteLine(myCard.Value);
+			Console.ReadLine();
+			*/
+
+			Console.OutputEncoding = System.Text.Encoding.Unicode;
+			DeckOfCards myDeck = new DeckOfCards();
+
+			//cycle thru deck
+			foreach (Card card in myDeck.Deck)
+			{
+				Console.WriteLine(card.Value + card.Suit);
+			}
+			Console.ReadLine();
+		}
+
+
+
+	}
+}
